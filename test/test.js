e7d86251fb635dfb8f3a7f70ca46a9552aa42634
Jessica Walter <153012670+jwalter03@users.noreply.github.com>
2024-10-31 14:38:51 +0000

    add branch

--- test/test.js
+++ test/test.js
@@ -5,10 +5,15 @@ describe('Regular fibonacci value', function() {
         assert.strictEqual(main.fibonacci(5), 5);
       });
 });
-
 describe('First two terms', function() {
-  it('should return 0 and 1 for first two terms', function() {
-      assert.strictEqual(main.fibonacci(0), 0);
-      assert.strictEqual(main.fibonacci(1), 1);
+    it('should return 0 and 1 for first two terms', function() {
+        assert.strictEqual(main.fibonacci(0), 0);
+        assert.strictEqual(main.fibonacci(1), 1);
+      });
+  });
+
+describe('Negative', function() {
+  it('should return undefined', function() {
+      assert.strictEqual(main.fibonacci(-1), undefined);
     });
 });
