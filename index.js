5d318baed2229ea5eccab1bbb8b7971725dff0bc
Jessica Walter <153012670+jwalter03@users.noreply.github.com>
2024-10-31 14:20:07 +0000

    initial files

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.fibonacci = function(n) {
+    if(n==0 || n==1)
+        return n;
+    return this.fibonacci(n-1) + this.fibonacci(n-2);
+}
+//change to factorial: n!=n*(n-1)!
+//fib(n)=fib(n-1)+fib(n-2)
